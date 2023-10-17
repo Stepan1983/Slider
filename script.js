9d94a8ec5a7fc68bc28b059238c2ca4c156b1872
Stepan1983 <80103271+Stepan1983@users.noreply.github.com>
2023-10-17 18:58:50 +0000

    Внес изменения в JS код

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ let checkboxButtons = document.querySelectorAll(".slider-checkbox-button");
 let slideImages = ["slider-intro1.png", "slider-intro2.png", "slider-intro3.png", "slider-intro4.png", "slider-intro5.png"];
 let currentIndex = 0;
 
-// Функция для изменения изображения
+
 function changeSlide() {
     sliderIntro.src = "img/" + slideImages[currentIndex];
 }
 
-// Обработчик клика для каждого checkbox
+
 checkboxButtons.forEach(function(checkbox, index) {
     checkbox.addEventListener("click", function() {
-        currentIndex = index; // Обновляем текущий индекс
-        changeSlide(); // Изменяем изображение
+        currentIndex = index; 
+        changeSlide(); 
     });
 });
